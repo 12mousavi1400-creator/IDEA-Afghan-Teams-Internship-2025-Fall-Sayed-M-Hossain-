6f50bb94d20d45eceb02f65d01b176ed497e43bf
12mousavi1400-creator <12mousavi1400@gmail.com>
2025-12-06 11:07:53 +0430

    Update question-2.php

--- week1/question-2.php
+++ week1/question-2.php
@@ -3,7 +3,21 @@ $num1 = 0;
 $num2 = 0;
 $sign = '';
 
-function calculate($num1, $sign, $num2)
+function factorial($n)
+{
+    if ($n < 0) {
+        return "Error: Factorial of negative number!";
+    }
+    if ($n == 0) return 1;
+
+    $result = 1;
+    for ($i = 1; $i <= $n; $i++) {
+        $result *= $i;
+    }
+    return $result;
+}
+
+function calculate($num1, $sign, $num2 = null)
 {
     switch ($sign) {
         case '+':
@@ -39,7 +53,10 @@ function calculate($num1, $sign, $num2)
             $result = ($num1 * $num2) / 100;
             echo "Result: $result";
             break;
+
+        case '!':
+            echo "Result:". factorial($num1);
     } 
 }
-calculate(12, '%', 2);
+calculate(4, '!');
 ?>
