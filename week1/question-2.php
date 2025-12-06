1005f61df63f300b054a8cdebecd0c7a170eb1e7
12mousavi1400-creator <12mousavi1400@gmail.com>
2025-12-06 10:26:07 +0430

    Update question-2.php

--- week1/question-2.php
+++ week1/question-2.php
@@ -1,1 +1,35 @@
+<?php
+$num1 = 0;
+$num2 = 0;
+$sign = '';
 
+function calculate($num1, $sign, $num2)
+{
+    switch ($sign) {
+        case '+':
+            $result = $num1 + $num2;
+            echo "Result: $result";
+            break;
+        case '-':
+            $result = $num1 - $num2;
+            echo "Result: $result";
+            break;
+
+        case '*':
+            $result = $num1 * $num2;
+            echo "Result: $result";
+            break;
+
+        case '/':
+            $result = $num1 / $num2;
+            echo "Result: $result";
+            break;
+
+        case '%':
+            $result = $num1 % $num2;
+            echo "Result: $result";
+            break;
+    } 
+}
+calculate(12, '%', 2);
+?>
